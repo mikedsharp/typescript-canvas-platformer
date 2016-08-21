adeb4017186ed414c8ed2cd6541162c72762dccb
Mike Sharp <msharp23@hotmail.co.uk>
2016-08-21 23:06:13 +0100

    added a way to change sprite type for player based on direction

---
+++ src/app/game/enums/playerAnimationTypes.ts
@@ -0,0 +1,13 @@
+export class PlayerAnimationTypes {
+
+  static animations = {
+    PLAYER_UP: 0,
+    PLAYER_DOWN: 1,
+    PLAYER_LEFT: 2,
+    PLAYER_RIGHT: 3
+  };
+
+  constructor(){
+  }
+
+}
--- src/app/game/systems/render.ts
+++ src/app/game/systems/render.ts
@@ -9,21 +9,49 @@ export class RenderSystem {
     return entity.components['size'] && entity.components['position'] && entity.components['sprite'];
   }
 
-  static invoke(entities, ctx:CanvasRenderingContext2D):void{
+  static invoke(entities, ctx:CanvasRenderingContext2D, dt:number):void{
 
     var filteredEntities = entities.filter(RenderSystem.meetsSystemRequisites)
 
     filteredEntities.forEach(function(entity){
+
+      let frame = {
+        currentFrame: 0,
+        animationType:0
+      };
+
+      if(entity.components['animation']){
+          frame.currentFrame = entity.components.animation.currentFrame;
+          frame.animationType = entity.components.animation.animationType;
+      }
+
       ctx.drawImage(
                     entity.components.sprite.image,
-                    0,
-                    0,
+                    frame.currentFrame * entity.components.size.width,
+                    frame.animationType * entity.components.size.height,
                     entity.components.size.width,
                     entity.components.size.height,
                     entity.components.position.x,
                     entity.components.position.y,
                     entity.components.size.width,
                     entity.components.size.height);
+
+      if(entity.components['animation'] && entity.components['animation'].animating ){
+
+           let currentTicks = new Date().getTime();
+           let pastTime = (currentTicks - entity.components.animation.lastIteration);
+
+           if(pastTime >=  entity.components.animation.updateRate){
+             entity.components.animation.currentFrame++;
+
+             if(entity.components.animation.currentFrame == entity.components.animation.frameCount){
+               entity.components.animation.currentFrame = 0;
+             }
+
+             entity.components.animation.lastIteration = currentTicks;
+           }
+      }
     });
+
   }
 }
